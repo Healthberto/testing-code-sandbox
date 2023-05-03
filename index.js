88cb1b23bc336123a1af5d670f5ad832e22e67ef
Gilberto Galvez <gilberto.galvez@gmail.com>
2023-05-03 02:21:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to my API, it is in progress yet !" });
 });
 
+app.get("/endpoint/", (req, res) => {
+  res.json({ message: "Welcome to my API, it is in progress yet !" });
+});
+
 // define a simple POST endpoint
 app.post("/", (req, res) => {
   const { name } = req.body;
